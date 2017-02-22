2eb30d702c362db2bfc0ebc991f19a0c65a63476
Jan Šimek <jsimek.cz@gmail.com>
2017-02-22 20:14:50 +0100

    emacs: added markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,18 @@
       '((".*" . "~/.emacs.d/autosaves/")))
 (setq auto-save-file-name-transforms
       '((".*" "~/.emacs.d/autosaves/" t)))
+
+;; Additional package repositories
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; live markdown preview: M-x flymd-flyit
+(require 'flymd)
+;; markdown-mode
+(require 'markdown-mode)
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
